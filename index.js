4b0a73bced72da2e2a081dd1e57f3f91119162d4
MrGuzior <konrad.guzior@gmail.com>
2023-01-24 13:27:49 +0100

    Remove utils

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 const fakeApi = require("./test/helpers/fake-api");
 const fakeFtp = require("./test/helpers/fake-ftp");
 const fakeGcs = require("./test/helpers/fake-gcs");
-const fakeS3 = require("./test/helpers/fake-s3");
-const fakeSes = require("./test/helpers/fake-ses");
 const fakeSftp = require("./test/helpers/fake-sftp");
 const fileUtils = require("./test/helpers/file-utils");
 const messageHelper = require("./test/helpers/message-helper");
@@ -22,13 +20,16 @@ module.exports = {
   fakeApi,
   fakeFtp,
   fakeGcs,
-  fakeS3,
-  fakeSes,
   fakeSftp,
   fileUtils,
   clone,
   messageHelper,
   pdfReader,
   requestHelper,
-  GCP: { assertHelpers, buildMessage, fakeCloudTask, fakeGcpAuth, fakePubSub, run },
+  assertHelpers,
+  buildMessage,
+  fakeCloudTask,
+  fakeGcpAuth,
+  fakePubSub,
+  run,
 };

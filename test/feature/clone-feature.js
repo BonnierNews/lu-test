d374be570e0dd550347d10ea9d3a859789285af3
Simon Varney <varney@users.noreply.github.com>
2023-07-19 17:04:49 +0200

    WIP fake-cloud-task-feature

--- test/feature/clone-feature.js
+++ test/feature/clone-feature.js
@@ -5,7 +5,7 @@ const object = { prop1: "text", prop2: [ { prop3: "deep" } ] };
 Feature("clone feature", () => {
   Scenario("successfully clone an object", () => {
     let response;
-    When("building a message with data", async () => {
+    When("cloning the object", async () => {
       response = await clone(object);
     });
     Then("we should have received a copy of the object", () => {
---
+++ test/feature/fake-cloud-task-feature.js
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+
+import * as fakeCloudTask from "../helpers/fake-cloud-task.js";
+
+const expectedExports = [
+  "enablePublish",
+  "fakeCreateTaskError",
+  "recordedMessageHandlerResponses",
+  "recordedMessages",
+  "reset",
+];
+
+describe("Exposed features", () => {
+  describe("Importing default export", () => {
+    it("The right stuff gets exposed", () => {
+      expect(Object.keys(fakeCloudTask).sort().join(",")).to.equal(
+        expectedExports.sort().join(",")
+      );
+    });
+  });
+});
+
+// TODO: write proper tests for this, once pubsub tests are written

a89669970f08c4d6ee8452946ea7e5227c60c513
Mattias Olla <mattias.olla@bonniernews.se>
2024-03-22 11:03:34 +0100

    Make fake cloud tasks behave more as a queue

--- test/feature/fake-cloud-tasks-feature.js
+++ test/feature/fake-cloud-tasks-feature.js
@@ -3,20 +3,27 @@ import { CloudTasksClient } from "@google-cloud/tasks";
 import config from "exp-config";
 import express from "express";
 
-import * as fakeCloudTask from "../helpers/fake-cloud-tasks.js";
+import * as fakeCloudTasks from "../helpers/fake-cloud-tasks.js";
 
-const expectedExports = [ "enablePublish", "recordedMessages", "recordedMessageHandlerResponses", "reset" ];
+const expectedExports = [
+  "enablePublish",
+  "runSequence",
+  "processMessages",
+  "recordedMessages",
+  "recordedMessageHandlerResponses",
+  "reset",
+];
 
 describe("fake-cloud-task Exposed features", () => {
   describe("Importing default export", () => {
     it("The right stuff gets exposed", () => {
-      expect(Object.keys(fakeCloudTask).sort().join(",")).to.equal(expectedExports.sort().join(","));
+      expect(Object.keys(fakeCloudTasks).sort().join(",")).to.equal(expectedExports.sort().join(","));
     });
   });
 });
 
 Feature("fake-cloud-task feature", () => {
-  beforeEachScenario(fakeCloudTask.reset);
+  beforeEachScenario(fakeCloudTasks.reset);
 
   Scenario("successfully create a task", () => {
     Given("we have enabled publish", async () => {
@@ -24,7 +31,7 @@ Feature("fake-cloud-task feature", () => {
       app.use(express.json());
       app.post("/foo/bar", (req, res) => res.status(200).json({ status: "ok" }).send());
 
-      await fakeCloudTask.enablePublish(app);
+      await fakeCloudTasks.enablePublish(app);
     });
 
     let cloudTask;
@@ -47,22 +54,27 @@ Feature("fake-cloud-task feature", () => {
       });
     });
 
+    And("the task has been run", async () => {
+      await fakeCloudTasks.processMessages();
+    });
+
     Then("we should have received the task name", () => {
       response.should.eql([ { name: "test-task" } ]);
     });
 
     And("we should have recorded the message", () => {
-      fakeCloudTask.recordedMessages()[0].should.eql({
+      fakeCloudTasks.recordedMessages()[0].should.eql({
         queue: config.cloudTasks.queue,
         httpMethod: "post",
-        body: { type: "something", id: "some-id" },
+        message: { type: "something", id: "some-id" },
         url: "/foo/bar",
         headers: { correlationId: "some-epic-id" },
+        correlationId: "some-epic-id",
       });
     });
 
     And("we should have recorded a message handler response", () => {
-      fakeCloudTask
+      fakeCloudTasks
         .recordedMessageHandlerResponses()[0]
         .should.deep.include({ body: { status: "ok" }, statusCode: 200 });
     });
@@ -74,7 +86,7 @@ Feature("fake-cloud-task feature", () => {
       app.use(express.json());
       app.get("/foo/bar", (req, res) => res.status(200).json({ status: "ok" }).send());
 
-      await fakeCloudTask.enablePublish(app);
+      await fakeCloudTasks.enablePublish(app);
     });
 
     let cloudTask;
@@ -96,23 +108,105 @@ Feature("fake-cloud-task feature", () => {
       });
     });
 
+    And("the task has been run", async () => {
+      await fakeCloudTasks.processMessages();
+    });
+
     Then("we should have received the task name", () => {
       response.should.eql([ { name: "test-task" } ]);
     });
 
     And("we should have recorded the message", () => {
-      fakeCloudTask.recordedMessages()[0].should.eql({
+      fakeCloudTasks.recordedMessages()[0].should.eql({
         queue: config.cloudTasks.queue,
         httpMethod: "get",
         url: "/foo/bar",
         headers: { correlationId: "some-epic-id" },
+        correlationId: "some-epic-id",
       });
     });
 
     And("we should have recorded a message handler response", () => {
-      fakeCloudTask
+      fakeCloudTasks
         .recordedMessageHandlerResponses()[0]
         .should.deep.include({ body: { status: "ok" }, statusCode: 200 });
+    });
+  });
+
+  Scenario("trying to run sequence without enabling publication", () => {
+    let error;
+    When("trying to process messages", async () => {
+      try {
+        await fakeCloudTasks.processMessages();
+      } catch (err) {
+        error = err;
+      }
+    });
+
+    Then("we should have received an error", () => {
+      error.message.should.eql("You must call `enablePublish` before processing messages");
+    });
+  });
+});
+
+Feature("cloud tasks run-sequence feature", () => {
+  beforeEachScenario(fakeCloudTasks.reset);
+
+  Scenario("successfully run a sequence", () => {
+    let app;
+    Given("a broker", () => {
+      app = express();
+      app.use(express.json());
+      app.post("/sequence", (req, res) => {
+        const cloudTask = new CloudTasksClient();
+        [ "task1", "task2" ].forEach((task) => {
+          cloudTask.createTask({
+            parent: config.cloudTasks.queue,
+            task: {
+              httpRequest: {
+                url: `${config.cloudTasks.selfUrl}/foo/bar`,
+                httpMethod: "post",
+                headers: { correlationId: "some-epic-id" },
+                body: Buffer.from(JSON.stringify({ task })),
+              },
+            },
+          });
+        });
+        res.status(200).json({ status: "ok" }).send();
+      });
+      app.post("/foo/bar", (req, res) => res.status(200).json({ task: req.body.task }).send());
+    });
+
+    let result;
+    When("running the sequence", async () => {
+      result = await fakeCloudTasks.runSequence(app, "/sequence");
+    });
+
+    Then("two messages should have been published", () => {
+      result.messages.should.eql([
+        {
+          message: { task: "task1" },
+          headers: { correlationId: "some-epic-id" },
+          httpMethod: "post",
+          queue: config.cloudTasks.queue,
+          url: "/foo/bar",
+          correlationId: "some-epic-id",
+        },
+        {
+          message: { task: "task2" },
+          headers: { correlationId: "some-epic-id" },
+          httpMethod: "post",
+          queue: config.cloudTasks.queue,
+          url: "/foo/bar",
+          correlationId: "some-epic-id",
+        },
+      ]);
+    });
+
+    And("the messages should have been processed", () => {
+      result.messageHandlerResponses
+        .map((response) => response.body)
+        .should.eql([ { task: "task1" }, { task: "task2" } ]);
     });
   });
 });
--- test/helpers/fake-cloud-tasks.js
+++ test/helpers/fake-cloud-tasks.js
@@ -2,10 +2,12 @@ import { createSandbox } from "sinon";
 import { CloudTasksClient } from "@google-cloud/tasks";
 import supertest from "supertest";
 import config from "exp-config";
+import { assert } from "chai";
 
 const sandbox = createSandbox();
 
-let stub;
+let stub, requestAgent;
+const messageQueue = []; // Will be used as the actual message queue
 const messages = [];
 const messageHandlerResponses = [];
 
@@ -17,44 +19,94 @@ function init() {
 
 export function enablePublish(broker) {
   init();
-  const requestAgent = supertest.agent(broker);
-
-  stub.createTask = async ({
-    parent,
-    task: { httpRequest: { httpMethod, headers, url, body } },
-  }) => {
-    const relativeUrl = url.replace(config.cloudTasks.selfUrl, "");
-    const queueName = parent.split("/").pop();
-    const bodyObject = body ? JSON.parse(body.toString()) : undefined;
-    const taskName = "test-task";
-
-    const cloudRunHeaders = {
-      "X-CloudTasks-QueueName": queueName,
-      "X-CloudTasks-TaskName": taskName,
-      "X-CloudTasks-TaskRetryCount": 0,
-      "X-CloudTasks-TaskExecutionCount": 1,
-      "X-CloudTasks-TaskETA": Date.now(),
-    };
-
-    messages.push({
-      queue: parent,
-      httpMethod,
-      headers,
-      url: relativeUrl,
-      ...(bodyObject && { body: bodyObject }),
-    });
-    const response = await requestAgent[httpMethod.toLowerCase()](relativeUrl)
-      .set({ ...headers, ...cloudRunHeaders })
-      .send(bodyObject);
-
-    messageHandlerResponses.push(response);
-    return [ { name: taskName } ];
+  requestAgent = supertest.agent(broker);
+  stub.createTask = (task) => {
+    messageQueue.push({ ...task, taskName: "test-task" });
+    return [ { name: "test-task" } ];
   };
+}
+
+/** @typedef {import("express").Express} Express */
+
+/** Run a full sequence, returning the first trigger HTTP response
+ * @param {Express} broker - The Express application that will run the sequence
+ * @param {String} url - The sequence or trigger URL to send the first request to
+ * @param {Object} body - The body of the first request
+ * @param {Object} headers - The headers of the first request
+ * @returns {Promise<{last: Object, triggeredFlows: string[], firstResponse: supertest.Response, messages: object[], messageHandlerResponses: object[]}>}
+ */
+export async function runSequence(broker, url, body, headers = {}) {
+  enablePublish(broker);
+  try {
+    const firstResponse = await requestAgent.post(url).set(headers).send(body);
+    await processMessages();
+
+    const last = messages.slice(-1)[0];
+    const triggeredFlows = [ ...new Set(recordedMessages().map((o) => o.url.split("/").slice(0, 2).join("."))) ];
+    return {
+      ...last,
+      triggeredFlows,
+      firstResponse,
+      messages: [ ...recordedMessages() ],
+      messageHandlerResponses: [ ...recordedMessageHandlerResponses() ],
+    };
+  } finally {
+    reset();
+  }
+}
+
+export async function processMessages() {
+  assert(requestAgent, "You must call `enablePublish` before processing messages");
+  while (messageQueue.length) {
+    const message = messageQueue.shift();
+    await handleMessage(message);
+  }
+}
+
+async function handleMessage({
+  parent,
+  task: { httpRequest: { httpMethod, headers, url, body } },
+  taskName,
+}) {
+  const relativeUrl = url.replace(config.cloudTasks.selfUrl, "");
+  const queueName = parent.split("/").pop();
+  const bodyObject = body ? JSON.parse(body.toString()) : undefined;
+
+  const cloudRunHeaders = {
+    "X-CloudTasks-QueueName": queueName,
+    "X-CloudTasks-TaskName": taskName,
+    "X-CloudTasks-TaskRetryCount": 0,
+    "X-CloudTasks-TaskExecutionCount": 1,
+    "X-CloudTasks-TaskETA": Date.now(),
+  };
+
+  messages.push({
+    queue: parent,
+    httpMethod,
+    headers,
+    url: relativeUrl,
+    ...(bodyObject && { message: bodyObject }),
+    correlationId: headers["correlation-id"] || headers.correlationId,
+  });
+  const response = await requestAgent[httpMethod.toLowerCase()](relativeUrl)
+    .set({ ...headers, ...cloudRunHeaders })
+    .send(bodyObject);
+
+  messageHandlerResponses.push({
+    statusCode: response.statusCode,
+    body: response.body,
+    headers: response.headers,
+    text: response.text,
+    error: response.error,
+    url: response.req.path,
+  });
 }
 
 export function reset() {
   messages.length = 0;
   messageHandlerResponses.length = 0;
+  messageQueue.length = 0;
+  requestAgent = undefined;
   sandbox.restore();
   stub = null;
 }
